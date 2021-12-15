9ab07dc4991b440fec3ea0cbaaee8bfe98fb39d3
quocthinh861 <quocthinh861@gmail.com>
2021-12-16 06:53:39 +0700

    .

---
+++ demos/Start_Here/MyShop.Web/Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Infrastructure;
+
+namespace MyShop.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ShoppingContext context;
+
+        public CustomerController()
+        {
+            context = new ShoppingContext();
+        }
+
+        public IActionResult Index(Guid? id)
+        {
+            if (id == null)
+            {
+                var customers = context.Customers.ToList();
+
+                return View(customers);
+            }
+            else
+            {
+                var customer = context.Customers.Find(id.Value);
+
+                return View(new[] { customer });
+            }
+        }
+    }
+}
---
+++ demos/Start_Here/MyShop.Web/Controllers/OrderController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyShop.Domain.Models;
+using MyShop.Infrastructure;
+using MyShop.Web.Models;
+
+namespace MyShop.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private ShoppingContext context;
+
+        public OrderController()
+        {
+            context = new ShoppingContext();
+        }
+
+        public IActionResult Index()
+        {
+            var orders = context.Orders
+                .Include(order => order.LineItems)
+                .ThenInclude(lineItem => lineItem.Product)
+                .Where(order => order.OrderDate > DateTime.UtcNow.AddDays(-1)).ToList();
+
+            return View(orders);
+        }
+
+        public IActionResult Create()
+        {
+            var products = context.Products.ToList();
+
+            return View(products);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateOrderModel model)
+        {
+            if (!model.LineItems.Any()) return BadRequest("Please submit line items");
+
+            if (string.IsNullOrWhiteSpace(model.Customer.Name)) return BadRequest("Customer needs a name");
+
+            var customer = new Customer
+            {
+                Name = model.Customer.Name,
+                ShippingAddress = model.Customer.ShippingAddress,
+                City = model.Customer.City,
+                PostalCode = model.Customer.PostalCode,
+                Country = model.Customer.Country
+            };
+
+            var order = new Order
+            {
+                LineItems = model.LineItems
+                    .Select(line => new LineItem { ProductId = line.ProductId, Quantity = line.Quantity })
+                    .ToList(),
+
+                Customer = customer
+            };
+
+            context.Orders.Add(order);
+
+            context.SaveChanges();
+
+            return Ok("Order Created");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
